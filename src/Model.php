c62a5401c5cd715d4f550a459dbf665134b4849f
Marijn Ophorst <marijn@sensimedia.nl>
2016-09-25 10:21:22 +0200

    typo

--- src/Model.php
+++ src/Model.php
@@ -43,7 +43,7 @@ trait Model
                         function ($match) {
                             return '_'.strtolower($match[1]);
                         },
-                        substr($method->getName, 3)
+                        substr($method->getName(), 3)
                     );
                     $callbacks[$name] = $method->getName();
                 }
