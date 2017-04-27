7dd15747b521cdd7ca70e0a1fb64e7c019989e8d
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-27 18:50:19 +0200

    test for property decorator

--- tests/Property.php
+++ tests/Property.php
@@ -3,6 +3,9 @@
 namespace Ornament\Tests;
 
 use Generator;
+use StdClass;
+use Ornament\Core\Model;
+use Ornament\Json;
 
 /**
  * Test the JSON decorator.
@@ -11,11 +14,27 @@ class Property
 {
     /**
      * A property annotated as Json should get decorated as the correct object
-     * {?}.
+     * {?} and retain its data {?}. After changing the JSON, we still have the
+     * updated information {?}. Serializing the property gives a `StdClass` {?},
+     * and a `__toString()` gives the serialized JSON.
      */
     public function testJsonDecoration() : Generator
     {
-        yield assert(true);
+        $model = new class() extends StdClass {
+            use Model;
+
+            /**
+             * @var Ornament\Json\Property
+             */
+            public $test;
+        };
+        $model->test = '{"foo":"bar"}';
+        yield assert($model->test instanceof Json\Property);
+        yield assert($model->test->foo == 'bar');
+        $model->test->bar = 'foo';
+        yield assert($model->test->bar == 'foo');
+        yield assert($model->test->jsonSerialize() instanceof StdClass);
+        yield assert("{$model->test}" == '{"foo":"bar","bar":"foo"}');
     }
 }
 
