ff60e8ed3c97f1d77c589337b71d43e621d3d3a2
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-27 17:23:22 +0200

    add even simpler exportAll trait

---
+++ src/ExportAll.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ornament\Json;
+
+use Ornament\Core\ModelCheck;
+use StdClass;
+
+trait ExportOnlyAll
+{
+    use ModelCheck {
+        ModelCheck::check as __ornamentExportAllCheck;
+    }
+    /**
+     * Returns a StdClass model representation suitable for Json serialization.
+     * This trait only exports both public and protected properties.
+     *
+     * @return StdClass
+     * @throws DomainException if called on a non-Ornament model
+     */
+    public function jsonSerialize() : StdClass
+    {
+        $this->__ornamentExportAllCheck();
+        $export = new StdClass;
+        foreach ($this->__state as $name => $value) {
+            $export->$name = $this->$name;
+        }
+        return $export;
+    }
+}
+
