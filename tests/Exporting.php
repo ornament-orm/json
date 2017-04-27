ee21aa538587ba002018f1f8a77ec49072d8fd2a
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-27 18:50:28 +0200

    setup tests for exporting

---
+++ tests/Exporting.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ornament\Tests;
+
+use Ornament\Core\Model;
+use Ornament\Json\SerializeOnlyPublic;
+use Ornament\Json\SerializeAll;
+use StdClass;
+
+/**
+ * Test exporting via the `jsonSerialize` traits.
+ */
+class Exporting
+{
+    /**
+     * With the correct trait applied, only public properties should be exported
+     * {?} and not protected ones {?}.
+     */
+    public function testExportsOnlyPublicProperties()
+    {
+        $model = new class() extends StdClass {
+            use Model;
+            use SerializeOnlyPublic;
+
+            public $foo = 1;
+            protected $bar = 2;
+        };
+        yield assert(isset($model->jsonSerialize()->foo));
+        yield assert(!isset($model->jsonSerialize()->bar));
+    }
+}
+
