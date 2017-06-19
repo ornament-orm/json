abfa4ec14831f09155feb8d879086ea951ecd61a
Marijn Ophorst <marijn@sensimedia.nl>
2017-06-19 11:12:23 +0200

    trait is gone

--- src/SerializeAll.php
+++ src/SerializeAll.php
@@ -2,23 +2,18 @@
 
 namespace Ornament\Json;
 
-use Ornament\Core\ModelCheck;
 use StdClass;
 
 trait SerializeAll
 {
-    use ModelCheck;
-
     /**
      * Returns a StdClass model representation suitable for Json serialization.
      * This trait only exports both public and protected properties.
      *
      * @return StdClass
-     * @throws DomainException if called on a non-Ornament model
      */
     public function jsonSerialize() : StdClass
     {
-        $this->__ornamentCheck();
         $export = new StdClass;
         foreach ($this->__state as $name => $value) {
             $export->$name = $this->$name;
--- src/SerializeOnlyPublic.php
+++ src/SerializeOnlyPublic.php
@@ -2,23 +2,18 @@
 
 namespace Ornament\Json;
 
-use Ornament\Core\ModelCheck;
 use StdClass;
 
 trait SerializeOnlyPublic
 {
-    use ModelCheck;
-
     /**
      * Returns a StdClass model representation suitable for Json serialization.
      * This trait only exports public properties.
      *
      * @return StdClass
-     * @throws DomainException if called on a non-Ornament model
      */
     public function jsonSerialize() : StdClass
     {
-        $this->__ornamentCheck();
         $annotations = $this->__ornamentalize();
         $export = new StdClass;
         foreach ($annotations['properties'] as $name => $anns) {
