cdc8d63d6e9687cc1ff597f7ec566d9bc53c4685
Marijn Ophorst <marijn@sensimedia.nl>
2018-02-02 14:33:32 +0100

    this is undesired

--- src/Property.php
+++ src/Property.php
@@ -5,15 +5,11 @@ namespace Ornament\Json;
 use Ornament\Core\Decorator;
 use JsonSerializable;
 use StdClass;
-use Iterator;
-use Countable;
-use ArrayAccess;
 use DomainException;
 
-class Property extends Decorator implements JsonSerializable, Iterator, Countable, ArrayAccess
+class Property extends Decorator implements JsonSerializable
 {
     private $decoded = null;
-    private $position = 0;
 
     public function __construct(StdClass $object, string $property)
     {
@@ -61,76 +57,6 @@ class Property extends Decorator implements JsonSerializable, Iterator, Countabl
     public function jsonSerialize()
     {
         return $this->decoded;
-    }
-
-    public function current()
-    {
-        $decoded = (array)$this->decoded;
-        return $decoded[array_keys((array)$this->decoded)[$this->position]];
-    }
-
-    public function key()
-    {
-        return array_keys((array)$this->decoded)[$this->position];
-    }
-
-    public function next()
-    {
-        ++$this->position;
-    }
-
-    public function rewind()
-    {
-        $this->position = 0;
-    }
-
-    public function valid()
-    {
-        return isset(array_keys((array)$this->decoded)[$this->position]);
-    }
-
-    public function count()
-    {
-        return count($this->decoded);
-    }
-
-    public function offsetExists($offset) : bool
-    {
-        if (is_object($this->decoded)) {
-            return isset($this->decoded->$offset);
-        } elseif (is_array($this->decoded)) {
-            return isset($this->decoded[$offset]);
-        }
-        return false;
-    }
-
-    public function offsetGet($offset)
-    {
-        if (is_object($this->decoded)) {
-            return $this->decoded->$offset;
-        } elseif (is_array($this->decoded)) {
-            return $this->decoded[$offset];
-        } else {
-            return null;
-        }
-    }
-
-    public function offsetSet($offset, $value)
-    {
-        if (is_object($this->decoded)) {
-            $this->decoded->$offset = $value;
-        } elseif (is_array($this->decoded)) {
-            $this->decoded[$offset] = $value;
-        }
-    }
-
-    public function offsetUnset($offset)
-    {
-        if (is_object($this->decoded)) {
-            unset($this->decoded->$offset);
-        } elseif (is_array($this->decoded)) {
-           unset($this->decoded[$offset]);
-        }
     }
 }
 
