0ece3403dd725de6ea430c27dafcb21dff0d14c3
Marijn Ophorst <marijn@monomelodies.nl>
2020-04-11 09:18:49 +0200

    serializer helper trait

---
+++ src/Serializer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ornament\Json;
+
+use ReflectionClass;
+
+trait Serializer
+{
+    private function serialized(int $flags)
+    {
+        $reflection = new ReflectionClass($this);
+        $export = new StdClass;
+        foreach ($reflection->getProperties($flags) as $property) {
+            $name = $property->getName();
+            $export->$name = $this->$name ?? null;
+        }
+        $cache = $this->__getModelPropertyDecorations();
+        foreach ($cache['methods'] as $prop => $getter) {
+            $export->$prop = $this->$getter($prop);
+        }
+        return $export;
+}
+
