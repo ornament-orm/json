4d01246ca88377370917e64673dd3a296d36d1e7
Marijn Ophorst <marijn@monomelodies.nl>
2020-04-11 09:24:56 +0200

    make cache static for optimization

--- src/Serializer.php
+++ src/Serializer.php
@@ -9,13 +9,16 @@ trait Serializer
 {
     private function jsonSerialized(int $flags) : stdClass
     {
+        static $cache;
+        if (!isset($cache)) {
+            $cache = $this->__getModelPropertyDecorations();
+        }
         $reflection = new ReflectionClass($this);
         $export = new StdClass;
         foreach ($reflection->getProperties($flags) as $property) {
             $name = $property->getName();
             $export->$name = $this->$name ?? null;
         }
-        $cache = $this->__getModelPropertyDecorations();
         foreach ($cache['methods'] as $prop => $getter) {
             $export->$prop = $this->$getter($prop);
         }
