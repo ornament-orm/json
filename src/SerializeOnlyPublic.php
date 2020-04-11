61dc4a7b0c9708d5921e0fd39b3f4596f8bd8006
Marijn Ophorst <marijn@monomelodies.nl>
2020-04-11 09:18:41 +0200

    abstract logic away into its own trait

--- src/SerializeOnlyPublic.php
+++ src/SerializeOnlyPublic.php
@@ -3,11 +3,12 @@
 namespace Ornament\Json;
 
 use stdClass;
-use ReflectionClass;
 use ReflectionProperty;
 
 trait SerializeOnlyPublic
 {
+    use Serializer;
+
     /**
      * Returns a stdClass model representation suitable for Json serialization.
      * This trait only exports public properties.
@@ -16,17 +17,7 @@ trait SerializeOnlyPublic
      */
     public function jsonSerialize() : stdClass
     {
-        $reflection = new ReflectionClass($this);
-        $export = new stdClass;
-        foreach ($reflection->getProperties(ReflectionProperty::IS_PUBLIC & ~ReflectionProperty::IS_STATIC) as $property) {
-            $name = $property->getName();
-            $export->$name = $this->$name ?? null;
-        }
-        $cache = $this->__getModelPropertyDecorations();
-        foreach ($cache['methods'] as $prop => $getter) {
-            $export->$prop = $this->$getter($prop);
-        }
-        return $export;
+        return $this->serialized(ReflectionProperty::IS_PUBLIC & ~ReflectionProperty::IS_STATIC);
     }
 }
 
